327683c9f8fc1ac77de99c0ce076711fae72cccd
Webvorks <webvork.nl@gmail.com>
2022-08-31 22:46:05 +0200

    nft card

---
+++ nft-preview-card-component-main/src/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(215, 51%, 70%)",
+        secondary: "hsl(178, 100%, 50%)",
+        white: "hsl(0, 0%, 100%)",
+        bg: "hsl(217, 54%, 11%)",
+        card: "hsl(216, 50%, 16%)",
+        line: "hsl(215, 32%, 27%)",
+      },
+    },
+  },
+};
